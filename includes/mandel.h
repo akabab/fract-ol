15cbcf455e664c958a9330d2084163304e9fecb5
ycribier <ycribier@student.42.fr>
2015-02-09 18:39:29 +0100

    opti + norme

--- includes/mandel.h
+++ includes/mandel.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandel.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycribier <ycribier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/09 17:52:18 by ycribier          #+#    #+#             */
+/*   Updated: 2015/02/09 18:06:48 by ycribier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MANDEL_H
 # define MANDEL_H
 
@@ -5,8 +17,8 @@
 # define MANDEL_ORIGIN_BI	-0.04
 # define MANDEL_ZOOM		0.7
 # define MANDEL_POWER		1
-# define MANDEL_COL1		0x4169E1
-# define MANDEL_COL2		0xE1B941
+# define MANDEL_COL1		0x30F2BB
+# define MANDEL_COL2		0xF23067
 
 typedef struct s_env	t_env;
 
--- src/thread.c
+++ src/thread.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycribier <ycribier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/09 17:52:18 by ycribier          #+#    #+#             */
+/*   Updated: 2015/02/09 18:30:42 by ycribier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fract.h"
 
 static void		*compute_part(void *p_data)
@@ -9,7 +21,7 @@ static void		*compute_part(void *p_data)
 
 	data = (t_th_data *)p_data;
 	y = data->start_y;
-	while (y < data->start_y + TH_N_LINE && y < W_HEIGHT)
+	while (y < W_HEIGHT)
 	{
 		x = 0;
 		while (x < W_WIDTH)
@@ -18,7 +30,7 @@ static void		*compute_part(void *p_data)
 			my_pixel_put_to_image(data->e->img, x, y, color);
 			x++;
 		}
-		y++;
+		y += TH_N_THREADS;
 	}
 	return (NULL);
 }
@@ -33,7 +45,7 @@ void			thread_compute_fract(t_env *e)
 	while (i < TH_N_THREADS)
 	{
 		data[i].e = e;
-		data[i].start_y = i * TH_N_LINE;
+		data[i].start_y = i;
 		if (pthread_create(&threads[i], NULL, compute_part, &data[i]) != 0)
 			exit(-1);
 		i++;
